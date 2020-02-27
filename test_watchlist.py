d8c4f3d66127bdc4037e034d90ab5c71c0ed3259
QYINAO <13233481207@163.com>
2020-02-27 18:58:26 +0800

    代码结构优化

--- test_watchlist.py
+++ test_watchlist.py
@@ -1,6 +1,7 @@
 import unittest
 
-from app import app,db,Movie,User
+from blog import app,db
+from blog.models import Movie,User
 
 class WatchlistTestCase(unittest.TestCase):
     def setUp(self):
